95435ff62e3dfc64c1972c63e96d25d795fd98bc
Akili Humphrey <akilihumphrey@gmail.com>
2019-02-01 02:12:48 +0000

    Automatically backed up by Learn

--- db/migrate/20190201020845_create_movies.rb
+++ db/migrate/20190201020845_create_movies.rb
@@ -1,6 +1,7 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
-    add_column :title , :director, :lead
-    
+    add_column :title , :director, :lead, :string
+    add_column :release_date, :integer
+    add_column :in_theaters, :boolean
   end
 end
